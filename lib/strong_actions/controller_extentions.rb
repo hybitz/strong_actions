48799976cce28f479a0f8fe4d2b4b10aff5fa575
ichy <ichylinux@gmail.com>
2014-08-31 11:48:47 +0900

    エラーメッセージを改善

--- lib/strong_actions/controller_extentions.rb
+++ lib/strong_actions/controller_extentions.rb
@@ -10,9 +10,11 @@ module StrongActions
     private
 
     def authorize_roles!
-      unless available?(controller_name, action_name, params)
-        message = "#{controller_name}##{action_name} is not permitted"
-        raise StrongActions::ForbiddenAction.new(message)
+      StrongActions.config.roles.each do |role|
+        unless judge(role, controller_name, action_name, params)
+          message = "#{controller_name}##{action_name} is not permitted for role #{role}"
+          raise StrongActions::ForbiddenAction.new(message)
+        end
       end
     end
 
@@ -20,33 +22,37 @@ module StrongActions
       action_name ||= 'index'
 
       StrongActions.config.roles.each do |role|
-        role_definition = StrongActions.config.role_definition(role)
-        next unless role_definition
-
-        begin
-          role_object = eval(role)
-        rescue NameError
-          raise "role #{role} is not defined in controller"
-        end
-
-        controller_value = role_definition[controller_name]
-        next if controller_value.nil?
-
-        if controller_value.is_a?(Hash)
-          action_value = controller_value[action_name]
-        else
-          action_value = controller_value
-        end
-        next if action_value.nil?
-
-        action_value = [action_value] unless action_value.is_a?(Array)
-        action_value.each do |definition|
-          return false unless definition
-          return false unless role_object.instance_eval(definition)
-        end
+        return false unless judge(role, controller_name, action_name, params)
       end
 
       true
+    end
+
+    def judge(role, controller_name, action_name, params)
+      role_definition = StrongActions.config.role_definition(role)
+      next unless role_definition
+
+      begin
+        role_object = eval(role)
+      rescue NameError
+        raise "role #{role} is not defined in controller"
+      end
+
+      controller_value = role_definition[controller_name]
+      next if controller_value.nil?
+
+      if controller_value.is_a?(Hash)
+        action_value = controller_value[action_name]
+      else
+        action_value = controller_value
+      end
+      next if action_value.nil?
+
+      action_value = [action_value] unless action_value.is_a?(Array)
+      action_value.each do |definition|
+        return false unless definition
+        return false unless role_object.instance_eval(definition)
+      end
     end
 
   end
